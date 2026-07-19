bb1233bac1554916804fe83c6ef8d34105905cfa
Tanaya Anap <tanayaanap@gmail.com>
2026-07-20 00:52:47 +0530

    Add products module with full CRUD functionality and migrations

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("customers/", include("customers.urls")),
+    path("products/", include("products.urls")),
+
 ]
